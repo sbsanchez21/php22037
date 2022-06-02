96268e3eeffa69764397db2cb8bd2831c2ff726f
santos <santos.sanchez@bue.edu.ar>
2022-06-02 12:03:16 -0300

    PHP Clase 1

---
+++ 07_phpPoo/Clase 1/clase/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Hola Mundo html</p>
+
+    <?php 
+        // conectarme a la Base de datos, traer los alumnos
+        // generar una tabla diníamica según los datos de alumnos
+
+        echo "<p style='color:blue'>Hola Mundo html</p>";
+        echo "<br>";
+        echo "<p>Hola Mundo html</p>";
+    ?>
+    
+</body>
+</html>
