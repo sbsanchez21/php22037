6feec3d2bf915a61e6249b1359a2457acc0e48bd
santos <santos.sanchez@bue.edu.ar>
2022-06-16 15:38:27 -0300

    PHP Clase 4

---
+++ 07_phpPoo/Clase 4/clase/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+</head>
+<body>
+    
+
+    <?php 
+    $usu = $_POST["usuario"];
+    $pass = $_POST["password"];
+    
+    // echo $usu;
+    // echo "<br>";
+    // echo $pass;
+
+    $mysqli=new mysqli("localhost","root","","alum22037");
+    $query=$mysqli->query("SELECT * FROM alumnos where usuario='$usu' AND password='$pass'");
+
+    if($query->fetch_assoc()) {
+        echo "<div class='text-primary'>Puede ingresar a la página</div>";
+    } else {
+        echo "<div class='text-danger'>Usuario o pass incorrectos</div>";
+    }
+
+    ?>
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+    
+</body>
+</html>
